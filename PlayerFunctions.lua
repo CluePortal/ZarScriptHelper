02177fc53ffd9c5937d6657a8f59430365530d14
CluePortal <130519058+CluePortal@users.noreply.github.com>
2023-07-30 21:56:25 -0700

    Update PlayerFunctions.lua

--- PlayerFunctions.lua
+++ PlayerFunctions.lua
@@ -26,27 +26,25 @@ local format = {
     reset = '\194\167r'
 }
 
-local getPlayer = {
-    function mouseOverBlock()
-        local n, v, x, y, z = player.over_mouse()
-        if n == 2 then
-            return v, x, y, z
+function mouseOverBlock()
+    local n, v, x, y, z = player.over_mouse()
+    if n == 2 then
+        return v, x, y, z
+    end
+    return nil
+end
+
+function findHotbarSlot(item)
+	for i = 1, 9 do
+        local name = player.inventory.item_information(35 + i)
+
+        if name ~= nil and string.match(name, item) then
+            return i
         end
-        return nil
     end
 
-    function findHotbarSlot(item)
-        for i = 1, 9 do
-            local name = player.inventory.item_information(35 + i)
-
-            if name ~= nil and string.match(name, item) then
-                return i
-            end
-        end
-
-        return -1
-    end
-}
+    return -1
+end
 
 function renderBlockSideOutline(minX, minY, minZ, maxX, maxY, maxZ, sc, r, g, b, a, thickness)
     red = r
