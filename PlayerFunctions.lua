c90048893815a286c98b5968c7ae53aa00aa469a
CluePortal <130519058+CluePortal@users.noreply.github.com>
2023-07-30 21:26:03 -0700

    Update PlayerFunctions.lua

--- PlayerFunctions.lua
+++ PlayerFunctions.lua
@@ -1,3 +1,19 @@
-function isGay()
-    client.print("Bruh")
+function mouseOverBlock()
+    local n, v, x, y, z = player.over_mouse()
+    if n == 2 then
+        return x, y, z
+    end
+    return nil
+end
+
+function findHotbarSlot(item)
+	for i = 1, 9 do
+        local name = player.inventory.item_information(35 + i)
+
+        if name ~= nil and string.match(name, item) then
+            return i
+        end
+    end
+
+    return -1
 end
